2ee2a078d091024328f4d56a827850f777d33037
Ninad Dhulap <ninadkdhulap17@outlook.com>
2025-01-09 22:23:13 +0530

    Add middleware for file uploads and error handling; configure CORS and cookie parsing

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import connectDB from "./db/index.js";
 import app from "./app.js";
 
 dotenv.config({
-  path: "./.env",
+    path: "./.env",
 });
 
 connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port: ${process.env.PORT}`);
-    });
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        });
 
-    // listening for the event of error
-    app.on("error", (error) => {
-      console.log("Error: ", error);
-      throw error;
+        // listening for the event of error
+        app.on("error", (error) => {
+            console.log("Error: ", error);
+            throw error;
+        });
+    })
+    .catch((error) => {
+        console.log("MONGODB connection failed !!! ", error);
     });
-  })
-  .catch((error) => {
-    console.log("MONGODB connection failed !!! ", error);
-  });
+    
