2cd36e79ee9f9044d61c8c63a22ebb76f2dacaa3
Ninad Dhulap <ninadkdhulap17@outlook.com>
2025-02-01 01:09:33 +0530

    Enhance user model: add refreshToken field, update isAlumni logic based on graduation year, and ensure password hashing on save

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,86 +1,104 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String,
+        }, // URL or path to avatar image
+        headline: {
+            type: String,
+            trim: true,
+        },
+        about: {
+            type: String,
+            trim: true,
+        },
+        socialHandles: {
+            type: Object,
+            of: String,
+        },
+        role: {
+            type: String,
+            enum: ["student", "faculty"],
+            required: true,
+            default: "student",
+        },
+        isAlumni: {
+            type: Boolean,
+            default: false,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        enrollmentYear: {
+            type: Number,
+        },
+        // expectedGraduationYear: {
+        //     type: Number,
+        // },
+        graduationYear: {
+            type: Number,
+            required: true,
+        },
+        branch: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        currentlyWorkingAt: {
+            type: String,
+        },
+        skills: {
+            type: [String],
+        },
+        refreshToken: {
+            type: String,
+        },
     },
-    password: { 
-        type: String, 
-        required: true 
-    },
-    firstName: { 
-        type: String, 
-        required: true,
-        trim: true,
-        index: true
-    },
-    lastName: { 
-        type: String, 
-        required: true,
-        trim: true,
-        index: true
-    },
-    avatar: { 
-        type: String,
-    }, // URL or path to avatar image
-    headline: { 
-        type: String,
-        trim: true
-    },
-    about: { 
-        type: String,
-        trim: true
-    },
-    socialHandles: {
-        type: Object,
-        of: String,
-    },
-    role: {
-        type: String,
-        enum: ["student", "faculty"],
-        required: true,
-        default: "student",
-    },
-    isAlumni: { 
-        type: Boolean, 
-        default: false 
-    },
-    isAdmin: { 
-        type: Boolean, 
-        default: false 
-    },
-    enrollmentYear: { 
-        type: Number 
-    },
-    // expectedGraduationYear: { 
-    //     type: Number,
-    // },
-    graduationYear: { 
-        type: Number,
-        required: true
-    },
-    branch: { 
-        type: String,
-        required: true,
-        trim: true
-    },
-    currentlyWorkingAt: { 
-        type: String 
-    },
-    skills: { 
-        type: [String] 
-    },
-}, { timestamps: true });
+    { timestamps: true }
+);
 
-userSchema.pre("save", async function(next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+    // Update isAlumni status based on graduation year
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1; // getMonth() returns 0-11
+
+    // Determine the threshold year for alumni status
+    const yearThreshold = currentMonth >= 5 ? currentYear : currentYear - 1;
+
+    if (this.graduationYear <= yearThreshold) {
+        this.isAlumni = true;
+    }
+
+    // Hash the password before saving the user model
+    if (!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
@@ -105,7 +123,7 @@ userSchema.methods.generateAccessToken = function () {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
         }
     );
-}
+};
 
 userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
